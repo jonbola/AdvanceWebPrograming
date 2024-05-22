1e5173dcd86949d1229821156f3c1f23e877d2ac
Jon <jonbola.vipx@gmail.com>
2024-05-22 17:00:21 +0700

    Create a switchable product list contains demo data

--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -1,11 +1,33 @@
-using Microsoft.AspNetCore.Mvc; 
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
-namespace SportsStore.Controllers { 
+namespace SportsStore.Controllers
+{
+    public class HomeController: Controller
+    {
 
- public class HomeController: Controller { 
+        public int PageSize = 4;
 
- public IActionResult Index() => View(); 
+        private IStoreRepository repository;
 
- } 
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
 
-} 
+        public ViewResult Index(int productPage = 1) => View(new ProductsListViewModel
+        {
+            Products = repository.Products
+            .OrderBy(p => p.ProductID)
+            .Skip((productPage - 1) * PageSize)
+            .Take(PageSize),
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = repository.Products.Count()
+            }
+        });
+    } 
+}
--- SportsSln/SportsStore/Program.cs
+++ SportsSln/SportsStore/Program.cs
@@ -1,13 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder(args); 
 
 builder.Services.AddControllersWithViews(); 
+builder.Services.AddDbContext<StoreDbContext>(opts => { opts.UseSqlServer( builder.Configuration["ConnectionStrings:SportsStoreConnection"]); });
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 
-var app = builder.Build(); 
+var app = builder.Build();
+app.UseStaticFiles();
+app.MapControllerRoute("pagination", "Products/Page{productPage}", new {Controller = "Home", action = "Index"});
+app.MapDefaultControllerRoute();
 
-//app.MapGet("/", () => "Hello World!"); 
+SeedData.EnsurePopulated(app);
 
-app.UseStaticFiles(); 
-
-app.MapDefaultControllerRoute(); 
-
-app.Run(); 
+app.Run();
